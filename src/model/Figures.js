6497aa0b50b7250f8c1e75200c1c597d976e08b2
Hukumka <hukumka@hukumka-X51H.(none)>
2014-10-18 12:09:50 +0400

    main models

---
+++ src/model/Figures.js
@@ -0,0 +1,230 @@
+module.exports = [
+    {
+        src : 'snowman.png',
+        title: 'Снеговик'
+    },
+    {
+        src : 'igloo.png',
+        title: 'Иглу'
+    },
+    {
+        src : 'sun.png',
+        title: 'Солнце'
+    },
+    {
+        src : 'maple.png',
+        title: 'Клен'
+    },
+    {
+        src : 'moon.png',
+        title: 'Луна'
+    },
+    {
+        src : 'treble_clef.png',
+        title: 'Скрипичный ключ'
+    },
+    {
+        src : 'skull_and_crossbones.png',
+        title: 'Череп и кости'
+    },
+    {
+        src : 'light.png',
+        title: 'Лампочка'
+    },
+    {
+        src : 'ghost.png',
+        title: 'Привидение'
+    },
+    {
+        src : 'lock.png',
+        title: 'Замок'
+    },
+    {
+        src : 'clock.png',
+        title: 'Часы'
+    },
+    {
+        src : 'jar_of_baby_food.png',
+        title: 'Баночка для детского питания'
+    },
+    {
+        src : 'bird.png',
+        title: 'Птица'
+    },
+    {
+        src : 'human.png',
+        title: 'Человек'
+    },
+    {
+        src : 'ladybug.png',
+        title: 'Божья коровка'
+    },
+    {
+        src : 'five.png',
+        title: 'Пять'
+    },
+    {
+        src : 'hammer.png',
+        title: 'Молоток'
+    },
+    {
+        src : 'scissors.png',
+        title: 'Ножницы'
+    },
+    {
+        src : 'zebra.png',
+        title: 'Зебра'
+    },
+    {
+        src : 'bomb.png',
+        title: 'Бомба'
+    },
+    {
+        src : 'cat.png',
+        title: 'Кот'
+    },
+    {
+        src : 'spider.png',
+        title: 'Паук'
+    },
+    {
+        src : 'lightning.png',
+        title: 'Молния'
+    },
+    {
+        src : 'cactus.png',
+        title: 'Кактус'
+    },
+    {
+        src : 'stop.png',
+        title: 'Стоп'
+    },
+    {
+        src : 'knight.png',
+        title: 'Шахмата'
+    },
+    {
+        src : 'ice.png',
+        title: 'Лед'
+    },
+    {
+        src : 'sight.png',
+        title: 'Прицел'
+    },
+    {
+        src : 'turtle.png',
+        title: 'Черепаха'
+    },
+    {
+        src : 'cheese.png',
+        title: 'Сыр'
+    },
+    {
+        src : 'exclamation_mark.png',
+        title: 'Восклицательный знак'
+    },
+    {
+        src : 'question_mark.png',
+        title: 'Вопросительный знак'
+    },
+    {
+        src : 'pencil.png',
+        title: 'Карандаш'
+    },
+    {
+        src : 'candle.png',
+        title: 'Свеча'
+    },
+    {
+        src : 'clover.png',
+        title: 'Клевер'
+    },
+    {
+        src : 'carrot.png',
+        title: 'Морковка'
+    },
+    {
+        src : 'dolphin.png',
+        title: 'Дельфин'
+    },
+    {
+        src : 'anchor.png',
+        title: 'Якорь'
+    },
+    {
+        src : 'daisy.png',
+        title: 'Ромашка'
+    },
+    {
+        src : 'tree.png',
+        title: 'Дерево'
+    },
+    {
+        src : 'fire.png',
+        title: 'Огонь'
+    },
+    {
+        src : 'apple.png',
+        title: 'Яблоко'
+    },
+    {
+        src : 'web.png',
+        title: 'Паутина'
+    },
+    {
+        src : 'dinosaur.png',
+        title: 'Динозавр'
+    },
+    {
+        src : 'dog.png',
+        title: 'Собака'
+    },
+    {
+        src : 'drop.png',
+        title: 'Капля'
+    },
+    {
+        src : 'snow.png',
+        title: 'Снежинка'
+    },
+    {
+        src : 'heart.png',
+        title: 'Сердце'
+    },
+    {
+        src : 'eye.png',
+        title: 'Глаз'
+    },
+    {
+        src : 'car.png',
+        title: 'Машина'
+    },
+    {
+        src : 'key.png',
+        title: 'Ключ'
+    },
+    {
+        src : 'dragon.png',
+        title: 'Дракон'
+    },
+    {
+        src : 'lips.png',
+        title: 'Губы'
+    },
+    {
+        src : 'glasses.png',
+        title: 'Очки'
+    },
+    {
+        src : '1.png',
+        title: 'Один'
+    },
+    {
+        src : '2.png',
+        title: 'Два'
+    },
+    {
+        src : '3.png',
+        title: 'Три'
+    }
+];
